ff84b919654cddf268aa1e62d5f06d64031d1126
Nacho <nacho.conso@gmail.com>
2022-11-07 19:30:59 -0300

    add privacy policy

---
+++ pages/privacy_policy.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function index() {
+  return (
+    <iframe
+      style={{ width: "100vw", height: "100vh" }}
+      src="https://www.freeprivacypolicy.com/live/7c302f12-cc97-42d5-950e-9e64a6db88b2"
+    ></iframe>
+  );
+}
